98a852ca63772ebc4d7f6693f5937fef4dff2de4
Braydon Harris <braydonharris@gmail.com>
2020-08-04 18:23:45 -0600

    Updating unit test Updating unit tests to use puppeteer

--- src/app/services/prismic.service.spec.ts
+++ src/app/services/prismic.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PrismicService } from './prismic.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PrismicService', () => {
   let service: PrismicService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(PrismicService);
   });
 
